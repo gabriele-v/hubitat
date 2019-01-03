785671cd1bce697493033ded393ca34ca3f1de07
Gabriele-V <gabriele@villanet.it>
2019-01-03 19:37:33 +0100

    [AlexaCookieNodeJs] Version 0.1.0

---
+++ AlexaCookieNodeJs/AlexaCookieNodeJs/AlexaCookie.js
@@ -0,0 +1,266 @@
+//
+//  Alexa Cookie NodeJS wrapper for Hubitat
+//
+//  Copyright 2018 Gabriele - Special thanks to Apollon77 for the original Alexia Cookie generator
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+//  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+//  for the specific language governing permissions and limitations under the License.
+//
+//  Change History:
+//
+//    Version   Date        Who             What
+//    -------   ----        ---             ----
+//     v0.1.0   2019-01-02  Gabriele        Initial Version
+
+
+'use strict';
+// Define libraries
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const fs = require('fs');
+
+const bodyParser = require('body-parser');
+const linkify = require('linkifyjs');
+require('linkifyjs/plugins/hashtag')(linkify);
+const linkifyHtml = require('linkifyjs/html');
+const uuidv4 = require('uuid/v4');
+
+const alexaCookie = require('./alexa-cookie/alexa-cookie');
+const _DictRefOptions = {}
+
+// Load config
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var port = process.env.PORT || config.port;
+var consoleLogging = config.consoleLogging;
+
+// Autenthication
+var auth = function (req, res, next) {
+    // Skip authentication if username is empty in config file
+    if (!config.username || config.username == '') {
+        return next();
+    }
+
+    // If authentication not provided, ask again
+    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="AlexaCookieNodeJs"');
+        res.end('Unauthorized');
+        return;
+    }
+
+    // Verify autenthication
+    const base64Credentials = req.headers.authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const [username, password] = credentials.split(':');
+    if (username === config.username && password === config.password) {
+        next();
+    } else {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="AlexaCookieNodeJs"');
+        res.end('Unauthorized');
+        return;
+    }
+}
+
+// Body parser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Socket.io
+io.sockets.on('connection', (socket) => {
+logToConsole("A user is connected to socket.io => " + socket);
+})
+app.use(function (req, res, next) {
+    req.io = io;
+    next();
+})
+
+// Index get
+app.get('/', auth, function (req, res) {
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>AlexaCookieNodeJs</title>
+        </head>
+        <body> 
+            <form action="/" method="post">
+                <fieldset>
+                    <legend>Amazon information:</legend>
+                    Username: <br>
+                    <input type="email" name="username" required><br>
+                    <br>
+                    Password:<br>
+                    <input type="password" name="password" required><br>
+                    <br>
+                    Country:<br>
+                    <input type="radio" name="amazonSite" value="alexa.amazon.com|en-US" checked> United States<br>
+                    <input type="radio" name="amazonSite" value="alexa.amazon.ca|en-US"> Canada<br>
+                    <input type="radio" name="amazonSite" value="amazon.co.uk|en-GB"> United Kingdom<br>
+                    <input type="radio" name="amazonSite" value="alexa.amazon.ca|en-US"> Italy<br>
+                    <br>
+                    <input type="submit" value="Submit">
+                </fieldset>
+            </form>
+            </body>
+            </html>
+    `);
+});
+
+// Index post
+app.post('/', auth, function (req, res) {
+    if (req.body.username == '' || req.body.password == '') {
+        res.send(`
+            Username and password are required!<br>
+            <br>
+            <button onclick="goBack()">Go Back</button>
+            <script> function goBack() { window.history.back(); }</script>
+        `);
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>AlexaCookieNodeJs</title>
+            <script src="/socket.io/socket.io.js"></script>
+        </head>
+        <body>  
+            <div id="text">Please wait...</div>
+            <script>
+                var socket = io();
+                socket.on('text', addText)
+                function addText(text){
+                    document.getElementById("text").innerHTML = text;
+                    addListners();
+                }
+                function addListners(){
+                    const list = document.getElementsByTagName("a");
+                    for (i = 0; i < list.length; ++i) {
+                        console.log(list[i]);
+                        list[i].onclick = function() {
+                            document.getElementById("text").innerHTML = "Please wait...";
+                        };
+                    }
+                }
+            </script>
+        </body>
+        </html>
+    `);
+
+    setTimeout(function () {
+        const options = {
+            logger: consoleLogging ? console.log : '',
+            amazonPage: req.body.amazonSite.split('|')[0],                   // optional: possible to use with different countries, default is 'amazon.de'
+            acceptLanguage: req.body.amazonSite.split('|')[1],               // optional: webpage language, should match to amazon-Page, default is 'de-DE'
+            userAgent: req.headers['user-agent'],                            // optional: own userAgent to use for all request, overwrites default one, should not be needed
+            proxyOnly: true,                                                 // optional: should only the proxy method be used? WHen no email/passwort are provided this will set to true automatically, default: false
+            setupProxy: true,                                                // optional: should the library setup a proxy to get cookie when automatic way did not worked? Default false!
+            proxyOwnIp: req.headers.host.split(':')[0],                      // required if proxy enabled: provide own IP or hostname to later access the proxy. needed to setup all rewriting and proxy stuff internally
+            proxyPort: config.proxyPort,                                     // optional: use this port for the proxy, default is 0 means random port is selected
+            proxyListenBind: '',                                             // optional: set this to bind the proxy to a special IP, default is '0.0.0.0'
+            proxyLogLevel: 'warn',                                           // optional: Loglevel of Proxy, default 'warn'
+            amazonPageProxyLanguage: req.body.amazonSite.split('|')[1],      // optional: language to be used for the Amazon Signin page the proxy calls. default is "de_DE")
+            formerRegistrationData: {}                                       // option/preferred: provide the result object from subsequent proxy usages here and some generated data will be reused for next proxy call too
+        };
+
+
+        alexaCookie.generateAlexaCookie(req.body.username, req.body.password, options, (err, result) => {
+            if (err) {
+                err = 'ERROR: ' + linkifyHtml(err.message) + '<br><br><br>If you already have inserted your credential in Amazon webpage, please wait...';
+                req.io.emit('text', err);
+            }
+            else if (result && result.csrf) {
+                alexaCookie.stopProxyServer();
+                var regData = JSON.stringify(result);
+                var text = `
+                    OAuth generated successfully!<br>
+                    Copy this data into Hubitat Alexa App<br>
+                    <br>
+                    <b>RefreshURL:</b><br>
+                    <textarea readonly rows="1" style="width:100%">http://${req.headers.host}/refreshCookie</textarea><br>
+                    <br>
+                    <b>RefreshOptions:</b><br>
+                    <textarea readonly rows="25" style="width:100%">${regData}</textarea>
+                `;
+                req.io.emit('text', text);
+            }
+        });
+    }, 2000);
+});
+
+app.post('/refreshCookie', auth, function (req, res) {
+    var refOptions = req.body;
+    if (!refOptions || refOptions == '' || Object.keys(refOptions).length == 0) {
+        res.status(400).send('Cookie options empty!');
+    }
+    else {
+        var guid = uuidv4()
+        logToConsole(`Refreshing cookie for guid ${guid} with Options: ${JSON.stringify(refOptions)}`);
+        res.send(guid);
+
+        const options = {
+            logger: consoleLogging ? console.log : '',
+            formerRegistrationData: refOptions // required: provide the result object from subsequent proxy usages here and some generated data will be reused for next proxy call too
+        };
+
+        alexaCookie.refreshAlexaCookie(options, (err, result) => {
+            if (err) {
+                logToConsole(`Cookie refresh error for guid ${guid} => Error: ${err.message}`);
+                _DictRefOptions[guid] = "ERROR: " + err.message;
+            }
+            else {
+                logToConsole(`Cookie refreshed succesfully for guid ${guid} => Result: ${JSON.stringify(result)}`);
+                _DictRefOptions[guid] = result;
+            }
+
+            // Remove it after 5 minutes, Hubitat download it's scheduled 2 minutes after refresh
+            setTimeout(function () {
+                if (_DictRefOptions[guid]) {
+                    delete _DictRefOptions[guid]
+                }
+            }, 1000 * 60 * 5);
+        });
+    }
+});
+
+app.get('/refreshCookie', auth, function (req, res) {
+    var guid = req.query.guid;
+    if (!guid || guid == '') {
+        logToConsole(`Guid empty for request ${req}!`);
+        res.status(400).send('Guid empty!');
+    }
+    else if (!_DictRefOptions[guid] || _DictRefOptions[guid] == '') {
+        logToConsole(`No cookie found for guid ${guid}!`);
+        res.status(500).send('No cookie or error for specified guid, maybe timeout of 5 mintues it\'s expired?');
+    }
+    else if (JSON.stringify(_DictRefOptions[guid]).includes("ERROR:")) {
+        logToConsole(`Cookie refresh error for guid ${guid}!`);
+        res.status(500).send(`Error refreshing cookie => ${_DictRefOptions[guid]}`);
+    }
+    else {
+        logToConsole(`Cookie sent succesfully for guid ${guid}!`);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(_DictRefOptions[guid])
+    }
+});
+
+server.listen(port, function () {
+    console.log(`AlexaCookieNodeJs listening on port ${port}!`);
+});
+
+function logToConsole(text) {
+    if (consoleLogging) {
+        console.log(text);
+    }
+}
---
+++ AlexaCookieNodeJs/AlexaCookieNodeJs/README.md
@@ -0,0 +1,40 @@
+﻿# Alexa Cookie NodeJS wrapper for Hubitat
+This webservice is a wrapper of orginal [Apollon77 alexa-cookie library](https://github.com/Apollon77/alexa-cookie) for use with Hubitat [Alexa TTS App](https://github.com/ogiewon/Hubitat/tree/master/Alexa%20TTS).
+
+# How to install
+(Tested on Ubuntu 16.04, commands may be a little different on other platforms)
+- Deploy a NodeJS server on your preferred platform (plenty of instuctions on web if needed)
+- It can be deployed on a cloud service as well, in that case it's suggested to configure authentication and don't publish the service but use it behind a HTTPS reverse proxy
+- Download latest version as zip and then unzip it on the server
+- Go to folder of AlexaCookieNodeJs
+- Make ```AlexaCookie.js``` executable (needed on Unix platforms only) => ```chmod +x ./AlexaCookie.js```
+- Execute the command ```npm install``` to install all required npm packages
+
+# Configuration
+In case default ports (wrapper on 81 and proxy on 82) have to be changed or to add authentication, modify the file ```config.json```
+Empty username and empty password means no authentication required
+```
+{
+	"port":"81",
+	"proxyPort":"82",
+	"username": "",
+	"password": "",
+	"consoleLogging": false
+}
+```
+
+# How to automatically start the wrapper
+- Execute command ```npm install -g pm2``` to install Node Process Manager 2 and allow autostart
+- Start the application with ```pm2 start AlexaCookie.js``` command
+- Execute command ```pm2 startup```
+- Execute command ```pm2 save```
+
+# Usage
+- Go to ```http://[serverip]:81```
+- Fill the form wih your Amazon username, password and country
+- At this point, depending on Amazon security, cookie refresh options could be immediately generated or a message will ask to go to webpage ```http://[serverip]:82``` (proxy) that will be opened in a new tab
+- **IMPORTANT!** do not close the original (let's call it :81) tab
+- Login again on second page (with 2FA if enabled)
+- **IMPORTANT!** After succesfull login on second webpage, a message will be prompted to close the browser: don't close the browser, but instead close only that tab (:82) and go back to the original tab (:81)
+- Copy the RefreshURL and RefreshOptions in the relative fields on Hubitat Alexa TTS App
+- Refresh will be now handled automatically between Hubitat and wrapper every 6 days, without human intervention requirement
